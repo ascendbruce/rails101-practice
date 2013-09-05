394ab8ac4e80e937041cd05d055d621dfcd0c3bd
Bruce Li <ascendbruce@gmail.com>
2013-09-05 18:00:03 +0800

    Rake task and seed for rebuilding project while developing.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# create first user as admin
+user_admin = User.new(
+  :email => "bruce@techbang.com.tw",
+  :password => "abcd1234",
+  :password_confirmation => "abcd1234"
+)
+user_admin.is_admin = true
+user_admin.save
+
+
+# create a test user, which is not an admin
+user = User.new(
+  :email => "bruce@gmail.com.tw",
+  :password => "abcd1234",
+  :password_confirmation => "abcd1234"
+)
+user.save
+
+
+# create deafult board
+board = Board.create!(
+  :name => "BBS"
+)
+
+# create default post
+post = board.posts.build(
+  :title => "Hello world",
+  :content => "Hi! This is your first post!\n\nWelcome!"
+)
+
+post.user = user_admin
+post.save!
+

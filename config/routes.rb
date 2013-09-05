e8a80ade8a09cf15af046d25a916e586d8be990f
Bruce Li <ascendbruce@gmail.com>
2013-09-05 16:25:45 +0800

    Remove trailing spaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails101Homework::Application.routes.draw do
-  
+
   devise_for :users
 
   resources :boards, :only => [:index, :show] do
--- db/migrate/20130903063504_create_posts.rb
+++ db/migrate/20130903063504_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.references :board
-      
+
       t.string :title
       t.text :content
 

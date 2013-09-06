7d30fa991d66552de3f9920e6b7b1190b614644b
Bruce Li <ascendbruce@gmail.com>
2013-09-06 10:17:44 +0800

    增加 counter_cache 的 migration 改用 reset_counters. Board.all 改用 board.find_each

--- db/migrate/20130905053159_add_posts_count_to_board.rb
+++ db/migrate/20130905053159_add_posts_count_to_board.rb
@@ -4,9 +4,8 @@ class AddPostsCountToBoard < ActiveRecord::Migration
   def up
     add_column :boards, :posts_count, :integer
 
-    #TODO: migration 內有code是可接受的嗎? && 先寫 assosication counter_cache 的話這個migration會爆炸
-    Board.all.each do |board|
-      board.update_attribute(:posts_count, board.posts.count)
+    Board.find_each do |board|
+      Board.reset_counters(board.id, :posts)
     end
   end
 

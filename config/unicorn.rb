ff83afe45886f9fcb327c36e846e865765b216b9
Bruce Li <ascendbruce@gmail.com>
2013-09-13 15:13:59 +0800

    fix path

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,6 +8,6 @@
 
 listen 2007 # by default Unicorn listens on port 8080
 worker_processes 2 # this should be >= nr_cpus
-pid "/path/to/app/shared/pids/unicorn.pid"
-stderr_path "/path/to/app/shared/log/unicorn.log"
-stdout_path "/path/to/app/shared/log/unicorn.log"
+pid         "/home/apps/rails101/shared/pids/unicorn.pid"
+stderr_path "/home/apps/rails101/shared/log/unicorn.log"
+stdout_path "/home/apps/rails101/shared/log/unicorn.log"
